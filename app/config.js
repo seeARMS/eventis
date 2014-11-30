4acb1e2d020bf101b05fd2ab1b37b6e635e18e35
Evan Marsland <ERmarsland@gmail.com>
2014-11-30 14:07:56 -0500

    updated conf

--- app/config.js
+++ app/config.js
@@ -6,6 +6,6 @@ module.exports = {
 	fb:{
 		id: '579321505535169',
 		secret: 'ff58be820ba2090e7a396720279bbfba',
-		callback: 'http://localhost:8080/fbauth/callback'
+		callback: 'http://104.236.5.205:8080/fbauth/callback'
 	}
 };
